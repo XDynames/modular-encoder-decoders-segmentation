2ec565a9b161872f97afe99ce09de78ba4896185
James Bockman <jpb@pop-os.localdomain>
2020-07-04 00:44:28 +0930

    Tested output strides and refinenet on camvid seems good

--- train_segmentation.sh
+++ train_segmentation.sh
@@ -4,6 +4,23 @@
 CUDA_VISIBLE_DEVICES="1" python train_segmentation.py \
 	--dataset_name camvid \
 	--dataset_dir ../seg_datasets/CamVid/ \
+	--decoder refinenet \
+	--encoder resnet_18 \
+	--imagenet True \
+	--batch_size 24 \
+	--val_batch_size  32 \
+	--val_interval 10 \
+	--lr 0.003 \
+	--decay 1e-5 \
+	--momentum 0.9 \
+	--num_epochs 300 \
+	--amp_level O2 \
+	--gradient_ckpt False \
+	--gpus "0"
+
+CUDA_VISIBLE_DEVICES="1" python train_segmentation.py \
+	--dataset_name camvid \
+	--dataset_dir ../seg_datasets/CamVid/ \
 	--decoder deeplab_v3+ \
 	--atrous_rates 6,12,18 \
 	--output_stride 16 \
